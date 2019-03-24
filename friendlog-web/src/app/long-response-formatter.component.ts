f6b43217502dd6a85558432a6dfa8934586c4755
Pandu Rendradjaja <prendradjaja@gmail.com>
2019-03-24 13:36:00 -0700

    Prettier

--- friendlog-web/src/app/long-response-formatter.component.ts
+++ friendlog-web/src/app/long-response-formatter.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { text } from '@angular/core/src/render3';
-import { LineToLineMappedSource } from 'webpack-sources';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-long-response-formatter',
-  templateUrl: './long-response-formatter.component.html',
-  styleUrls: ['./long-response-formatter.component.scss']
+  selector: "app-long-response-formatter",
+  templateUrl: "./long-response-formatter.component.html",
+  styleUrls: ["./long-response-formatter.component.scss"]
 })
 export class LongResponseFormatterComponent implements OnInit {
-
   @Input() text;
 
   paragraphs: string[][];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.paragraphs = this.text.split('\n\n').map(para => para.split('\n'));
+    this.paragraphs = this.text.split("\n\n").map(para => para.split("\n"));
   }
 }
