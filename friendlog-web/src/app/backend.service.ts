95defe53f5be1861e1203ee210c9a5301b2388c8
Pandu Rendradjaja <prendradjaja@gmail.com>
2019-03-24 21:55:05 -0700

    Custom colors!

--- friendlog-web/src/app/backend.service.ts
+++ friendlog-web/src/app/backend.service.ts
@@ -1,41 +1,77 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class BackendService {
-
   private parser = new Parser();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // todo instead of making this silly get/getCached interface, use an observable(??)
   public getCached() {
-    const cachedData = localStorage.getItem('bookworm/cached-data');
+    const cachedData = localStorage.getItem("bookworm/cached-data");
     if (cachedData) {
       const res = JSON.parse(cachedData);
-      return this.parser.parse(res['values'] as string[][]);
+      return this.parser.parse(res["values"] as string[][]);
     }
   }
 
+  // todo add return type for this and other get methods
   public get() {
-    const API_KEY=localStorage.getItem('bookworm/google-api-key');
+    const API_KEY = localStorage.getItem("bookworm/google-api-key");
     if (API_KEY) {
-      const RANGE='A1:G500';
-      const SPREADSHEET_ID='1dZGi9Vw5ReO3Lh2ebosuA6U0lxU71DqeavLuOudhMBI';
-      const url=`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
-      return this.http.get(url).toPromise().then(
-        res => {
-          localStorage.setItem('bookworm/cached-data', JSON.stringify(res));
-          return this.parser.parse(res['values'] as string[][]);
-        },
-        err => {
-          window.alert('Error fetching from db');
-          return [];
-        }
-      );
+      const RANGE = "A1:G500";
+      const SPREADSHEET_ID = "1dZGi9Vw5ReO3Lh2ebosuA6U0lxU71DqeavLuOudhMBI";
+      const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
+      return this.http
+        .get(url)
+        .toPromise()
+        .then(
+          res => {
+            localStorage.setItem("bookworm/cached-data", JSON.stringify(res));
+            return this.parser.parse(res["values"] as string[][]);
+          },
+          err => {
+            window.alert("Error fetching from db");
+            return [];
+          }
+        );
+    } else {
+      return Promise.reject();
+    }
+  }
+
+  // todo dedupe colors get code
+  public getColorsCached() {
+    const cachedData = localStorage.getItem("bookworm/cached-colors");
+    if (cachedData) {
+      const res = JSON.parse(cachedData);
+      return this.parser.parseColors(res["values"] as string[][]);
+    }
+  }
+
+  public getColors() {
+    const API_KEY = localStorage.getItem("bookworm/google-api-key");
+    if (API_KEY) {
+      const RANGE = "Colors!A1:B500";
+      const SPREADSHEET_ID = "1dZGi9Vw5ReO3Lh2ebosuA6U0lxU71DqeavLuOudhMBI";
+      const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
+      return this.http
+        .get(url)
+        .toPromise()
+        .then(
+          res => {
+            localStorage.setItem("bookworm/cached-colors", JSON.stringify(res));
+            return this.parser.parseColors(res["values"] as string[][]);
+          },
+          err => {
+            window.alert("Error fetching from db");
+            return {};
+          }
+        );
     } else {
       return Promise.reject();
     }
@@ -47,8 +83,14 @@ export class Row {
   // - Row.headers
   // - Row's attributes
   // - Parser.parseRow
-  static headers = ["Timestamp","What book?","Start place (optional)","End place","Notes"];
-  createdAt: string;  // When was the entry recorded?]
+  static headers = [
+    "Timestamp",
+    "What book?",
+    "Start place (optional)",
+    "End place",
+    "Notes"
+  ];
+  createdAt: string; // When was the entry recorded?]
   book: string;
   start: string;
   end: string;
@@ -56,10 +98,8 @@ export class Row {
 }
 
 class Parser {
-  
-
   parse(rows: string[][]): Row[] {
-    if (! this.headerEqualsExpected(rows)) {
+    if (!this.headerEqualsExpected(rows)) {
       return null;
     }
     const ret = [];
@@ -69,7 +109,7 @@ class Parser {
     return ret;
   }
   parseRow(row: string[]): Row {
-    const [A,B,C,D,E] = [0,1,2,3,4];
+    const [A, B, C, D, E] = [0, 1, 2, 3, 4];
 
     const ret = new Row();
     ret.createdAt = row[A];
@@ -77,6 +117,18 @@ class Parser {
     ret.start = row[C];
     ret.end = row[D];
     ret.notes = row[E];
+    return ret;
+  }
+
+  // todo name this type
+  parseColors(rows: string[][]): { [book: string]: string } {
+    const ret = {};
+    rows.forEach(([book, color]) => {
+      if (color) {
+        ret[book] = color;
+      }
+    });
+    console.log(ret);
     return ret;
   }
 
--- friendlog-web/src/app/color.service.ts
+++ friendlog-web/src/app/color.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from "@angular/core";
+import { BackendService } from "./backend.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class ColorService {
-  constructor() {}
+  chosenColors: { [book: string]: string };
+
+  constructor(private backendService: BackendService) {
+    this.chosenColors = {};
+    const cached = this.backendService.getColorsCached();
+    if (cached) {
+      this.chosenColors = cached;
+    }
+    this.backendService.getColors().then(colors => {
+      this.chosenColors = colors;
+    });
+  }
 
   /**
    * Adapted from https://stackoverflow.com/a/16348977
    */
   public getColor(book: string): string {
-    if (book === "The Gamer (s1)") {
-      return "#5aa8c6";
+    if (this.chosenColors.hasOwnProperty(book)) {
+      return this.chosenColors[book];
     }
     var hash = 0;
     for (var i = 0; i < book.length; i++) {
